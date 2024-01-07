143e44d8473d633db9f376067024ec8f4d9cdd21
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2024-01-07 20:00:25 +0530

    props and prop drilling

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
-const Header = () => {
-  // const headerStyle = {
-  //   backgroundColor: 'royalblue', 
-  //   color: '#fff'
-  // }
-
+const Header = ({title}) => {
   return (
-    // <header style={headerStyle}>
     <header>
-        <h1>Groceries List</h1>
+        <h1>{title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: "Default Title"
 }
 
 export default Header
